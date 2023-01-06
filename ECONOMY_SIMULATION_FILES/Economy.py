20bd30764f570439f02f1b1d2a24ce20b7d87105
LocalHomieKB <122056476+LocalHomieKB@users.noreply.github.com>
2023-01-06 20:09:43 +0000

    Improving data inputter function, more comments

--- ECONOMY_SIMULATION_FILES/Economy.py
+++ ECONOMY_SIMULATION_FILES/Economy.py
@@ -12,8 +12,6 @@ This is equal to:TOTAL_SCORE = 90348800000 VALUE_OF_TOWN = 1129360000
 """
 
 import random
-from math import sin
-from math import log
 
 WORKING_NAME = "Simulacra" # One simulation should be based on the economy of Greece before during and after the Troika program
 POPULATION = 5000
@@ -26,15 +24,12 @@ def SCOPE(INPUT_SCOPE):
     "If Weekly the program runs for a year with weekly updates of 52"\
     "If Yearly the programs runs for 20 years with yearly updates of 20")
 
-PACE = input("Slow or Fast or rapid Pace"\
-    "If slow then data updates every 3 seconds"\
-    "If fast then data updates every 0.5 seconds"\
-    "If rapid then data has no update and simply gives you final results ASAP(for data collection")
 def CRIME(CRIME_RATE):
     if SCOPE == YEAR:
         CRIME = 520
     elif SCOPE == WEEK:
         CRIME = 52 # criminal events a year, converitble to weeks if needed
+
 def datainputter(IMMIGRATION_PERCENT,MIGRATION_PERCENT,TAXES,POPULATION,WORK_EFFICIENCY,
             PUBLIC_SPENDING,MIGRATION,IMMIGRATION,WORKING_AGE_POPULATION,UNEMPLOYED_POPULATION,
             RICH_POPULATION,RICH_POPULATION_VALUE,WORKING_POPULATION_VALUE,TOTAL_SALARIES_PAID,
@@ -47,45 +42,56 @@ def datainputter(IMMIGRATION_PERCENT,MIGRATION_PERCENT,TAXES,POPULATION,WORK_EFF
                        #Mortality goes down, values which impact it positvely also are impacted positvely
                        #By it. Negatively impacted by crime, mortality, migration of skilled workers(not yet
                        # implemented) Unemployment decreases it directly. 
-        CRIME = (1520 - (LITERACY*125) )
+        CRIME = (1520 - (LITERACY*125) ) #The result is 520, the base crime rate of 520 crimes a year, 10 a week.
+                                         #This works out nicely for any increase in literacy (beyond 0.8)decreasing crime
+                                         #and vice versa   .
         #1000 can represent the base weighted value figure of variable importance. Anything else less important goes below this.
         IMMIGRATION_PERCENT = 1000 * LITERACY + 1000 
         MIGRATION_PERCENT = 1000 * CRIME
         IMMIGRATION = 1000 * IMMIGRATION_PERCENT
-        MIGRATION
+        MIGRATION = 1000 * MIGRATION_PERCENT                #All these variables are in progress, edit as you like but comment your logic             
         YEARLY_MORTALITY_RATE = CRIME*0.15*POPULATION + 0.02*POPULATION
         POPULATION = (IMMIGRATION-MIGRATION) - YEARLY_MORTALITY_RATE
         #Immigration percent to be weighed according to importance. Initially it will be:
         #1.00 for most importance, 0.8 for high, 0.5 for medium, 0.3 for low, 0 for no importance e.t.c
         IMMIGRATION_PERCENT=0.16*LITERACY
+"""
+Developing the datainputter function to a usable and eventually an advanced state akin to a simulation of Vic 2
+#Immigration_percent is dependent on literacy,GDP,employment_rate (lower=high_immigration)
+# total_welfare(higher better), taxes.
+#In order of importance highest to lowest. With high mid and low being referred to.
+#Immigrants will be defined as separate classes of high, mid, low and refugee.
+#for lower class population: total welfare, literacy, taxes, employment_rate, GDP
+# for middle class population: employment_rate, GDP, literacy, taxes, total welfare
+# for upper class population: literacy, taxes, employment_rate, GDP, total welfare
+# For immigrants the categories of desire are:
+# for lower class population: employment_rate, total welfare, literacy, taxes, GDP
+# for middle class population: employment_rate, employment_rate(intentional double),
+# literacy, taxes, GDP,total welfare
+# for upper class population: employment_rate(relative to base e.g. assume the home countries
+# employment rate to be base, unless theyre 3rd country in which case a separate variable is necessary
+# , 3rd countries will have a minute upper attraction. Its guiding features will be,
+# the country attractiveness relevant to the home and tourist country,
+# So consider what affects immigration as what is better about this country in comparison to that one,
+# Ideally 3 variables will exist. Neighbour, continent, interglobal. Typed in order
+# of relative immigration amounts. Even if employment is high a 3rd world country
+# will only have so many people capable of purchasing VISA's etc. One change will be 
+# upper populations have refugees as well as the other regions. 3rd class will have highest
+# , mid will have a very small amount apart from criminal asylums. The high class
+# may have more appearance in rich countries due to tax asylums and/or other fraud related 
+# crimes. literacy, taxes, GDP, total welfare
+# for refugee class population: employment_rate, total welfare, literacy, taxes, GDP
+# employment must be more important for middle than low, welfare most for low, taxes for the rich,
+#  are highest then middle and low are the same. GDP high, mid, low, lit high, mid, low.
+#  For most variables, desirability is in order of which class you desire.
+"""
+#Beginning of program load
+PACE = input("Slow or Fast or rapid Pace"\
+    "If slow then data updates every 3 seconds"\
+    "If fast then data updates every 0.5 seconds"\
+    "If rapid then data has no update and simply gives you final results ASAP(for data collection")
 
-            #Immigration_percent is dependent on literacy,GDP,employment_rate
-            # (lower=high_immigration) total_welfare(higher better), taxes.
-            #  In order of importance highest to lowest. With high mid and low being referred to.
-            #  Immigrants will be defined as separate classes of high, mid, low and refugee.
-            # for lower class population: total welfare, literacy, taxes, employment_rate, GDP
-            # for middle class population: employment_rate, GDP, literacy, taxes, total welfare
-            # for upper class population: literacy, taxes, employment_rate, GDP, total welfare
-            # For immigrants the categories of desire are:
-            # for lower class population: employment_rate, total welfare, literacy, taxes, GDP
-            # for middle class population: employment_rate, employment_rate(intentional double),
-            # literacy, taxes, GDP,total welfare
-            # for upper class population: employment_rate(relative to base e.g. assume the home countries
-            # employment rate to be base, unless theyre 3rd country in which case a separate variable is necessary
-            # , 3rd countries will have a minute upper attraction. Its guiding features will be,
-            # the country attractiveness relevant to the home and tourist country,
-            # So consider what affects immigration as what is better about this country in comparison to that one,
-            # Ideally 3 variables will exist. Neighbour, continent, interglobal. Typed in order
-            # of relative immigration amounts. Even if employment is high a 3rd world country
-            # will only have so many people capable of purchasing VISA's etc. One change will be 
-            # upper populations have refugees as well as the other regions. 3rd class will have highest
-            # , mid will have a very small amount apart from criminal asylums. The high class
-            # may have more appearance in rich countries due to tax asylums and/or other fraud related 
-            # crimes. literacy, taxes, GDP, total welfare
-            # for refugee class population: employment_rate, total welfare, literacy, taxes, GDP
-            # employment must be more important for middle than low, welfare most for low, taxes for the rich,
-            #  are highest then middle and low are the same. GDP high, mid, low, lit high, mid, low.
-            #  For most variables, desirability is in order of which class you desire.
+#Archivable data for consulting of future ideas
 DATA_INPUTTED=0
 MODE = str(input("Input mode "))
 while DATA_INPUTTED < 2:
@@ -138,7 +144,7 @@ while DATA_INPUTTED < 2:
             POPULATION += 3000
         elif 20 <= Crisis <= 40:
             POPULATION -= 3000
-
+#File save code for data analysis
 """os.chdir("/workspaces/Economy-Program/EconomySimulationFiles")
 DATA_INPUTTED = 0
 with open("File_Number.txt", "r+", encoding="utf-8") as fp:
