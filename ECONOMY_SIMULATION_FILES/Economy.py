91aa0510066df62e6f159ce6a295c748457e94f9
LocalHomieKB <122056476+LocalHomieKB@users.noreply.github.com>
2023-01-06 14:24:27 +0000

    Cosmetic changes
    Added triple speech at the top comments.

--- ECONOMY_SIMULATION_FILES/Economy.py
+++ ECONOMY_SIMULATION_FILES/Economy.py
@@ -1,18 +1,20 @@
 """
 Provides an in depth simulation of an economy akin to the UK
 """
-# Goals for this program:
-# A realistic simulation of a city - Not achieved
-# The average score for the game is equal to playing it with no tax tampering whatsoever
-# This is equal to:TOTAL_SCORE = 90348800000
-# VALUE_OF_TOWN = 1129360000
-# Your success in the simulation is based on your total score as well as your final year score
-# Do better in these than in a simulation with no changes added, and you will be a winner
-# The only variable that needs changing should be the OS directory. Usually a subfolder exists
-# below the economy folder but my understanding of GitHub prevents me from doing this.
-# Any questions should be directed to my GitHub project. Constructive criticism is appreciated
-# , especially from a data analysis standpoint as that is the direction this project is
-# going towards
+
+"""
+Goals for this program:
+A realistic simulation of a city - Not achieved
+The average score for the game is equal to playing it with no tax tampering whatsoever
+This is equal to:TOTAL_SCORE = 90348800000 VALUE_OF_TOWN = 1129360000
+Your success in the simulation is based on your total score as well as your final year score
+Do better in these than in a simulation with no changes added, and you will be a winner
+The only variable that needs changing should be the OS directory. Usually a subfolder exists
+below the economy folder but my understanding of GitHub prevents me from doing this.
+Any questions should be directed to my GitHub project. Constructive criticism is appreciated
+especially from a data analysis standpoint as that is the direction this project is going towards
+"""
+
 import os
 import random
 os.chdir("/workspaces/Economy-Program/EconomySimulationFiles")
