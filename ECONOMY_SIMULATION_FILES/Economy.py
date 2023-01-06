e82e7db7e8a69a7aef2a4954a73642524535de17
LocalHomieKB <122056476+LocalHomieKB@users.noreply.github.com>
2023-01-06 17:44:44 +0000

    Massive changhes:
    File system removed
    New and improved function system in the works. Including
    immigration, mortality and all new wealth levels

--- ECONOMY_SIMULATION_FILES/Economy.py
+++ ECONOMY_SIMULATION_FILES/Economy.py
@@ -1,25 +1,90 @@
 """
-Provides an in depth simulation of an economy akin to the UK
+An interactive tech demo to teach economics.
 Goals for this program:
-A realistic simulation of a city - Not achieved
-The average score for the game is equal to playing it with no tax tampering whatsoever
-This is equal to:TOTAL_SCORE = 90348800000 VALUE_OF_TOWN = 1129360000
 Your success in the simulation is based on your total score as well as your final year score
 Do better in these than in a simulation with no changes added, and you will be a winner
 The only variable that needs changing should be the OS directory. Usually a subfolder exists
 below the economy folder but my understanding of GitHub prevents me from doing this.
 Any questions should be directed to my GitHub project. Constructive criticism is appreciated
 especially from a data analysis standpoint as that is the direction this project is going towards
+The average score for the game is equal to playing it with no tax tampering whatsoever
+This is equal to:TOTAL_SCORE = 90348800000 VALUE_OF_TOWN = 1129360000
 """
-import os
+
 import random
+from math import sin
+from math import log
+
+WORKING_NAME = "ShoreLands"
+POPULATION = 5000
+WEEKLY_UPDATES = False
+YEARLY_UPDATES = False
+
+#Scan the variable INPUT_SCOPE before the program runs to 
+def SCOPE(INPUT_SCOPE):
+    INPUT_SCOPE = input("Choose either Weekly or Yearly"\
+    "If Weekly the program runs for a year with weekly updates of 52"\
+    "If Yearly the programs runs for 20 years with yearly updates of 20")
+
+PACE = input("Slow or Fast or rapid Pace"\
+    "If slow then data updates every 3 seconds"\
+    "If fast then data updates every 0.5 seconds"\
+    "If rapid then data has no update and simply gives you final results ASAP(for data collection")
+def CRIME(CRIME_RATE):
+    if SCOPE == YEAR:
+        CRIME = 520
+    if SCOPE == WEEK:
+        CRIME = 52 # criminal events a year, converitble to weeks if needed
+def datainputter(IMMIGRATION_PERCENT,MIGRATION_PERCENT,TAXES,POPULATION,WORK_EFFICIENCY,
+            PUBLIC_SPENDING,MIGRATION,IMMIGRATION,WORKING_AGE_POPULATION,UNEMPLOYED_POPULATION,
+            RICH_POPULATION,RICH_POPULATION_VALUE,WORKING_POPULATION_VALUE,TOTAL_SALARIES_PAID,
+            TOTAL_WELFARE,TOTAL_SCORE,VALUE_OF_TOWN,YEAR,CRIME,LITERACY,GDP,YEARLY_MORTALITY_RATE):
+    YEAR=1960
+    while YEAR<2020 or KeyboardInterrupt:
+        YEAR+=1
+        LITERACY = 0.8 * ((sin(60)*GDP))*(log(30)*RICH_POPULATION) + (YEAR*0.025)
+        CRIME = 
+        IMMIGRATION_PERCENT = 1000 * LITERACY + 1000 
+        MIGRATION_PERCENT = 1000 * CRIME
+        IMMIGRATION = 1000 * IMMIGRATION_PERCENT
+        MIGRATION
+        YEARLY_MORTALITY_RATE = CRIME*0.15*POPULATION + 0.02*POPULATION
+        POPULATION = (IMMIGRATION-MIGRATION) - YEARLY_MORTALITY_RATE
+        #Immigration percent to be weighed according to importance. Initially it will be:
+        #1.00 for most importance, 0.8 for high, 0.5 for medium, 0.3 for low, 0 for no importance e.t.c
+        IMMIGRATION_PERCENT=0.16*LITERACY
+
+            #Immigration_percent is dependent on literacy,GDP,employment_rate
+            # (lower=high_immigration) total_welfare(higher better), taxes.
+            #  In order of importance highest to lowest. With high mid and low being referred to.
+            #  Immigrants will be defined as separate classes of high, mid, low and refugee.
+            # for lower class population: total welfare, literacy, taxes, employment_rate, GDP
+            # for middle class population: employment_rate, GDP, literacy, taxes, total welfare
+            # for upper class population: literacy, taxes, employment_rate, GDP, total welfare
+            # For immigrants the categories of desire are:
+            # for lower class population: employment_rate, total welfare, literacy, taxes, GDP
+            # for middle class population: employment_rate, employment_rate(intentional double),
+            # literacy, taxes, GDP,total welfare
+            # for upper class population: employment_rate(relative to base e.g. assume the home countries
+            # employment rate to be base, unless theyre 3rd country in which case a separate variable is necessary
+            # , 3rd countries will have a minute upper attraction. Its guiding features will be,
+            # the country attractiveness relevant to the home and tourist country,
+            # So consider what affects immigration as what is better about this country in comparison to that one,
+            # Ideally 3 variables will exist. Neighbour, continent, interglobal. Typed in order
+            # of relative immigration amounts. Even if employment is high a 3rd world country
+            # will only have so many people capable of purchasing VISA's etc. One change will be 
+            # upper populations have refugees as well as the other regions. 3rd class will have highest
+            # , mid will have a very small amount apart from criminal asylums. The high class
+            # may have more appearance in rich countries due to tax asylums and/or other fraud related 
+            # crimes. literacy, taxes, GDP, total welfare
+            # for refugee class population: employment_rate, total welfare, literacy, taxes, GDP
+            # employment must be more important for middle than low, welfare most for low, taxes for the rich,
+            #  are highest then middle and low are the same. GDP high, mid, low, lit high, mid, low.
+            #  For most variables, desirability is in order of which class you desire.
 DATA_INPUTTED=0
 MODE = str(input("Input mode "))
 while DATA_INPUTTED < 2:
     YEAR = 1960
-    CHOICE = 0
-    CHOICE1 = 0
-    CHOICE2 = 0
     IMMIGRATION_PERCENT = 0.016
     MIGRATION_PERCENT = 0.00037
     TAXES = 0.05
