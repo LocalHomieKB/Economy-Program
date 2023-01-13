4f5c538bd232e8548d85c6e627650a2bacb259c7
LocalHomieKB <122056476+LocalHomieKB@users.noreply.github.com>
2023-01-13 20:26:20 +0000

    creating the ui

--- ECONOMY_SIMULATION_FILES/ECONOMYUIinprogress.py
+++ ECONOMY_SIMULATION_FILES/ECONOMYUIinprogress.py
@@ -4,14 +4,23 @@ root = Tk()
 root.title('Display1')
 root.geometry("600x400")
 
-def printSomething():
+def taxrateprint():
     # if you want the button to disappear:
     # button.destroy() or button.pack_forget()
     label = Label(root, text= "0.05")
     #this creates a new label to the GUI
     label.pack() 
 
-button = Button(root, text="Current Tax Rate", command=printSomething) 
+def populationprint():
+    # if you want the button to disappear:
+    # button.destroy() or button.pack_forget()
+    label = Label(root, text= "5000")
+    #this creates a new label to the GUI
+    label.pack() 
+
+button = Button(root, text="Current Tax Rate", command=taxrateprint) 
+button.pack()
+button2 = Button(root, text="Current Tax Rate", command=populationprint) 
 button.pack()
 
 root.mainloop()
