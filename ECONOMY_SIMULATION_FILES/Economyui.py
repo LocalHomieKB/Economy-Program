43ddbfc15c985c21b2cb03761a0f4d56873b30a7
LocalHomieKB <122056476+LocalHomieKB@users.noreply.github.com>
2023-01-15 12:23:19 +0000

    Update Economyui.py

--- ECONOMY_SIMULATION_FILES/Economyui.py
+++ ECONOMY_SIMULATION_FILES/Economyui.py
@@ -1,27 +1,52 @@
-from tkinter import *
+from tkinter import * #Standard library
+from tkinter import ttk #New themed widgets, to access the themed versions:
+                    #ttk.entry for example is required rather than entry
+                    #As entry would give you the tk version
+                    #Best advice is to prefix with ttk
 
-root = Tk()
-root.title('Display1')
-root.geometry("1920x1080")
+def calculate(*args): # straightforward calculation
+    try:
+        value = float(feet.get())
+        meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
+        #Multiplying and dividiing by a thousand is to prevent rounding 
+        #Problems
+    except ValueError:
+        pass
 
-def taxrateprint():
-    # if you want the button to disappear:
-    # button.destroy() or button.pack_forget()
-    label = Label(root, text= "0.05")
-    #this creates a new label to the GUI
-    label.pack() 
-    button.destroy()
+root = Tk() # application window
+root.title("Feet to Meters") # title (duh)
 
-def populationprint():
-    # if you want the button to disappear:
-    # button.destroy() or button.pack_forget()
-    label = Label(root, text= "5000")
-    #this creates a new label to the GUI
-    label.pack() 
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S)) # places the frame in 
+                                                    #our main applciation window
+root.columnconfigure(0, weight=1) # frame fills any space resized
+root.rowconfigure(0, weight=1) # frame fills any space is resized
+ 
+feet = StringVar() # Creates widget
+feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet) #
+# Places feet_entry inside the parent, the content frame known as ttk.entry
+#Width defines how many characters wide it should appear
+feet_entry.grid(column=2, row=1, sticky=(W, E))
+#Defines the row and column it will be placed in. Sticky defines how it 
+#should line up
+meters = StringVar() # label that display meters to calculate
+ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+#places the meters label
+ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, 
+             row=3, sticky=W)
+#the calculate button which defines the calculate function's action
+ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
+ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
+ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+#The last 3 labels make the program easier to use and are visual
 
-button = Button(root, text="Current Tax Rate", command=taxrateprint) 
-button.pack()
-button2 = Button(root, text="Current Tax Rate", command=populationprint) 
-button.pack()
+for child in mainframe.winfo_children(): 
+    child.grid_configure(padx=5, pady=5)
+#This puts padding around each widget to prevent them being scrunced
+# #supposdely this could be bypassed by putting it into grid, but
+#But this is a shortcut
+
+feet_entry.focus() # cursor starts on the entry widget
+root.bind("<Return>", calculate) # if return key is pressed it calculate
 
 root.mainloop()
